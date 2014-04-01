cabfeafd8e56c53dd2eb0f6bd295f1b5750af42e
liuyu <42625647@qq.com>
2014-04-01 11:34:34 +0800

    Create Project

---
+++ LvHttMutiButton/gen/com/example/lvhttmutibutton/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.lvhttmutibutton;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ LvHttMutiButton/src/com/example/lvhttmutibutton/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.lvhttmutibutton;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	
+	private Button btn;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+		btn = (Button)this.findViewById(R.id.button1);
+	
+		btn.setOnTouchListener(listner);
+	}
+
+	private View.OnTouchListener listner = new View.OnTouchListener() {
+		
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			// TODO Auto-generated method stub
+//			if(event.getAction() == MotionEvent.ACTION_DOWN)
+//			{
+//				v.setBackgroundResource(R.drawable.drawer_icon_self_service);
+//			}
+//			else
+//			{
+//				v.setBackgroundResource(R.drawable.drawer_icon_group);
+//			}
+			return false;
+		}
+	};
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			Log.i("","no exit");
+		}
+		return false;
+	}
+ 
+
+}
